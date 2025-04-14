50d61f167b843ce904e750784cdd67af7fe74621
sumantra04 <sumantrab.04@gmail.com>
2025-04-15 02:21:01 +0530

    StarBackground.tsx @types/random import modified

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,7 +3,8 @@
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+const random: any = require('maath/random/dist/maath-random.esm');
+
 
 const StarBackground = (props: any) => {
   const ref: any = useRef();
