65c952d04bf86336a7171e285a29f2be5b163a5d
sumantra04 <sumantrab.04@gmail.com>
2025-04-16 00:30:35 +0530

    Unnecessary files removed and some merged to one

---
+++ components/sub/SkillText.tsx
@@ -0,0 +1,28 @@
+{/*"use client"
+import React from 'react'
+import {motion} from 'framer-motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/24/solid'
+
+const SkillText = () => {
+  return (
+    <div className='w-full h-auto flex flex-col items-center justify-center'>
+<motion.div
+          variants={slideInFromTop}>
+        </motion.div>
+        <motion.h2
+        initial={{ opacity: 0, x: -120 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+        className="text-5xl md:text-6xl text-white font-extrabold mb-16 text-center">
+        <span className="bg-gradient-to-r from-cyan-500 to-pink-500 text-transparent bg-clip-text">
+        Skills and Tech Stack
+        </span>
+        </motion.h2>
+        <motion.div
+        variants={slideInFromRight(0.5)}>
+        </motion.div>
+    </div>
+  )
+}
+export default SkillText*/}
