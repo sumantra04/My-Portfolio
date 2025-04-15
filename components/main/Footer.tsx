e597ae1c8a69e8513a90fa4a4067733c56a839be
sumantra04 <sumantrab.04@gmail.com>
2025-04-15 14:07:18 +0530

    footer background made transparent

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10 bg-black text-white relative z-[50]" id="contact">
+    <footer className="w-full pt-20 pb-10 text-white relative z-[50]" id="contact">
       <div className="flex flex-col items-center px-4">
         <h1 className="text-center text-4xl sm:text-5xl font-extrabold leading-tight">
           Looking for a{" "}
@@ -19,23 +19,23 @@ const Footer = () => {
         <p className="text-white/70 text-center text-base md:text-lg max-w-xl mt-5 md:mt-8">
           If you're building something great and need a reliable developer, let’s connect and bring your vision to life.
         </p>
+
         <div className="mt-6">
           <a
-          href="https://mail.google.com/mail/?view=cm&fs=1&to=sumantrab.04@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="relative z-[60]"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=sumantrab.04@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative z-[60]"
           >
-          <button
-            className="flex items-center justify-center bg-gradient-to-r from-cyan-500 to-pink-500 text-white px-6 py-3 rounded-lg mt-6 transition-all duration-300 hover:scale-105 focus:outline-none"
-            aria-label="Compose email to work together"
+            <button
+              className="flex items-center justify-center bg-gradient-to-r from-cyan-500 to-pink-500 text-white px-6 py-3 rounded-lg mt-6 transition-all duration-300 hover:scale-105 focus:outline-none"
+              aria-label="Compose email to work together"
             >
-      <FaLocationArrow className="mr-2" />
-      Let’s work together
-    </button>
-  </a>
-</div>
-
+              <FaLocationArrow className="mr-2" />
+              Let’s work together
+            </button>
+          </a>
+        </div>
       </div>
 
       <div className="flex flex-col md:flex-row justify-between items-center px-6 mt-16">
@@ -52,7 +52,7 @@ const Footer = () => {
               aria-label={`Visit ${info.name}`}
               className="cursor-pointer relative z-[60]"
             >
-              <div className="w-12 h-12 bg-black rounded-lg flex justify-center items-center hover:bg-white/10 transition-all duration-300">
+              <div className="w-12 h-12 rounded-lg flex justify-center items-center hover:bg-white/10 transition-all duration-300">
                 <Image
                   src={info.src}
                   alt={`${info.name} icon`}
